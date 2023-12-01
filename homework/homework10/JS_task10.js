ee95e7e45566b3ef88abbe3b6a4af73a89931beb
LiliaBilous <liliyabilous.email@gmail.com>
2023-12-01 16:25:28 +0200

    fished icons state&summit alerts

--- homework/homework10/JS_task10.js
+++ homework/homework10/JS_task10.js
@@ -1,30 +1,54 @@
 const passwordInput = document.querySelector("#passwordInput");
-const confirmPasswordInput = document.querySelector('#confirmPasswordInput');
-const icons = document.querySelectorAll(".fas");
+const confirmPasswordInput = document.querySelector("#confirmPasswordInput");
+const iconShowPassword = document.querySelector("#iconShowPassword");
+const iconHidePassword = document.querySelector("#iconHidePassword");
 
-for(let icon of icons) {
-    icon.addEventListener("click", () => {
-        
-        if(passwordInput.type === "password") {
-            passwordInput.type = "text";
-            icon.classList.remove("fa-eye");
-            icon.classList.add('fa-eye-slash');
-        }
-        //  else {
-        //     passwordInput.type = "password";
-        //     icon.classList.remove('fa-eye-slash');
-        //     icon.classList.add('fa-eye');
-        // }      
-    });
-}
+iconShowPassword.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    passwordInput.setAttribute("type", "text");
+    iconShowPassword.classList.remove("fa-eye");
+    iconShowPassword.classList.add("fa-eye-slash");
+  } else {
+    iconShowPassword.classList.remove("fa-eye-slash");
+    iconShowPassword.classList.add("fa-eye");
+    passwordInput.setAttribute("type", "password");
+  }
+});
+iconHidePassword.addEventListener("click", () => {
+  if (confirmPasswordInput.type === "password") {
+    confirmPasswordInput.setAttribute("type", "text");
+    iconHidePassword.classList.remove("fa-eye");
+    iconHidePassword.classList.add("fa-eye-slash");
+  } else {
+    confirmPasswordInput.setAttribute("type", "password");
+    iconHidePassword.classList.remove("fa-eye-slash");
+    iconHidePassword.classList.add("fa-eye");
+  }
+});
 
 const form = document.querySelector("form");
+const checkbox = document.querySelector(".checkbox");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if(passwordInput === confirmPasswordInput) {
-    console.log(`You are welcome`);
+  if (passwordInput.value === confirmPasswordInput.value) {
+    alert(`You are welcome`);
   } else {
-    console.log(`Потрібно ввести однакові значення`);
+    let alertMessage = document.createElement(`div`);
+    alertMessage.className = "alert";
+    alertMessage.innerHTML = "Потрібно ввести однакові значення";
+    checkbox.before(alertMessage);
   }
 });
+
+// function togglePasword(input, icon) {
+//   if(input.type === 'password') {
+//     input.setAttribute('type', 'text');
+//     icon.classList.remove('fa-eye');
+//     icon.classList.add('fa-eye-slash');
+//   } else {
+//     icon.classList.remove('fa-eye-slash');
+//     icon.classList.add('fa-eye');
+//     input.setAttribute('type', 'password');
+//   }
+// }
