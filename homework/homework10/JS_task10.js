87d29f6da706350bb9a091deb6cee69ee8828f24
LiliaBilous <liliyabilous.email@gmail.com>
2023-12-02 16:29:50 +0200

    changed toggle function&validate confirm password

--- homework/homework10/JS_task10.js
+++ homework/homework10/JS_task10.js
@@ -1,54 +1,40 @@
 const passwordInput = document.querySelector("#passwordInput");
 const confirmPasswordInput = document.querySelector("#confirmPasswordInput");
-const iconShowPassword = document.querySelector("#iconShowPassword");
-const iconHidePassword = document.querySelector("#iconHidePassword");
+const passwordIcon = document.querySelector("#passwordIcon");
+const confirmPasswordIcon = document.querySelector("#confirmPasswordIcon");
 
-iconShowPassword.addEventListener("click", () => {
-  if (passwordInput.type === "password") {
-    passwordInput.setAttribute("type", "text");
-    iconShowPassword.classList.remove("fa-eye");
-    iconShowPassword.classList.add("fa-eye-slash");
+passwordIcon.addEventListener("click", togglePasword);
+confirmPasswordIcon.addEventListener("click", togglePasword);
+
+function togglePasword(event) {
+  if(event.target.previousSibling.type === 'password') {
+    event.target.classList.remove('fa-eye');
+    event.target.classList.add('fa-eye-slash');
+    event.target.previousSibling.setAttribute('type', 'text');
   } else {
-    iconShowPassword.classList.remove("fa-eye-slash");
-    iconShowPassword.classList.add("fa-eye");
-    passwordInput.setAttribute("type", "password");
+    event.target.classList.remove('fa-eye-slash');
+    event.target.classList.add('fa-eye');
+    event.target.previousSibling.setAttribute('type', 'password');
   }
-});
-iconHidePassword.addEventListener("click", () => {
-  if (confirmPasswordInput.type === "password") {
-    confirmPasswordInput.setAttribute("type", "text");
-    iconHidePassword.classList.remove("fa-eye");
-    iconHidePassword.classList.add("fa-eye-slash");
-  } else {
-    confirmPasswordInput.setAttribute("type", "password");
-    iconHidePassword.classList.remove("fa-eye-slash");
-    iconHidePassword.classList.add("fa-eye");
-  }
-});
+}
 
 const form = document.querySelector("form");
 const checkbox = document.querySelector(".checkbox");
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-
-  if (passwordInput.value === confirmPasswordInput.value) {
+  
+  let alertMessage = document.querySelector('.alert');
+  if (passwordInput.value === '' || confirmPasswordInput.value === '') {
+    alertMessage.innerHTML = "Потрібно ввести значення";
+  } else if (passwordInput.value === confirmPasswordInput.value) {
+    let oldAlert = document.querySelector('.alert');
+    if (oldAlert) {
+      oldAlert.remove();
+    }
     alert(`You are welcome`);
   } else {
-    let alertMessage = document.createElement(`div`);
-    alertMessage.className = "alert";
     alertMessage.innerHTML = "Потрібно ввести однакові значення";
-    checkbox.before(alertMessage);
   }
+  
 });
-
-// function togglePasword(input, icon) {
-//   if(input.type === 'password') {
-//     input.setAttribute('type', 'text');
-//     icon.classList.remove('fa-eye');
-//     icon.classList.add('fa-eye-slash');
-//   } else {
-//     icon.classList.remove('fa-eye-slash');
-//     icon.classList.add('fa-eye');
-//     input.setAttribute('type', 'password');
-//   }
-// }
