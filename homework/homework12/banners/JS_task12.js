3b82be462c1b77887b685399eacf3297e4a4cc81
LiliaBilous <liliyabilous.email@gmail.com>
2023-12-13 21:00:40 +0200

    deleted extra foreach loop

--- homework/homework12/banners/JS_task12.js
+++ homework/homework12/banners/JS_task12.js
@@ -1,19 +1,17 @@
 const imageCollection = document.querySelectorAll(".image-to-show");
 const imageCollectionLength = imageCollection.length;
-let currentIndex = 0;
+let currentIndex = 1;
 
 function changePhoto() {
     imageCollection.forEach((image, index) => {
-        imageCollection.forEach((image, index) => {
-            if (index === currentIndex) {
-                image.classList.add('active');
-            } else {
-                image.classList.remove('active');
-            }
-        });
-        currentIndex = (currentIndex + 1) % imageCollectionLength;
-        console.log(currentIndex)
-    })
+        if (index === currentIndex) {
+            image.classList.add('active');
+        } else {
+            image.classList.remove('active');
+        }
+    });
+    currentIndex = (currentIndex + 1) % imageCollectionLength;
+    console.log(currentIndex)
     
 }
-setTimeout(changePhoto, 10000);
+setInterval(changePhoto, 10000);
