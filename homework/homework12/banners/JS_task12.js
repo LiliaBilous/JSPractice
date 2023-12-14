225e3df91b3ef7f473c3fb210386dd5aa1dba84d
LiliaBilous <liliyabilous.email@gmail.com>
2023-12-14 15:28:24 +0200

    added timer function

--- homework/homework12/banners/JS_task12.js
+++ homework/homework12/banners/JS_task12.js
@@ -1,17 +1,61 @@
 const imageCollection = document.querySelectorAll(".image-to-show");
 const imageCollectionLength = imageCollection.length;
 let currentIndex = 1;
+let intervalID;
+let timerTime = 10;
+let isRunning = false;
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
 
 function changePhoto() {
-    imageCollection.forEach((image, index) => {
-        if (index === currentIndex) {
-            image.classList.add('active');
-        } else {
-            image.classList.remove('active');
-        }
-    });
-    currentIndex = (currentIndex + 1) % imageCollectionLength;
-    console.log(currentIndex)
-    
+  imageCollection.forEach((image, index) => {
+    if (index === currentIndex) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+  currentIndex = (currentIndex + 1) % imageCollectionLength;
 }
-setInterval(changePhoto, 10000);
+
+function updateTimer() {
+  minutes.textContent = Math.floor(timerTime / 60)
+    .toString()
+    .padStart(2, "0");
+  seconds.textContent = (timerTime % 60).toString().padStart(2, "0");
+}
+
+function stopShow() {
+  clearInterval(intervalID);
+  isRunning = false;
+  // intervalID = null;
+}
+function startShow() {
+  intervalID = setInterval(() => {
+    if (!isRunning) {
+      changePhoto();
+      timerTime = 10;
+      updateTimer();
+    }
+  }, 10000);
+}
+
+function restartShow() {
+  isRunning = false;
+  startShow();
+}
+
+document.querySelector(".stop-show").addEventListener("click", () => {
+  stopShow();
+  isRunning = true;
+});
+document.querySelector(".start-show").addEventListener("click", restartShow);
+
+startShow();
+
+setInterval(() => {
+  if (timerTime > 0 && !isRunning) {
+    timerTime--;
+    updateTimer();
+  }
+}, 1000);
