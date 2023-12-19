3573c417f988f8b41cc90e958f5e3f94bb7143fe
LiliaBilous <liliyabilous.email@gmail.com>
2023-12-19 23:07:44 +0200

    add header tabs-menu

--- homework/homework14/js/JS_task14.js
+++ homework/homework14/js/JS_task14.js
@@ -1,10 +1,18 @@
+// function switchTab(tabIndex) {
+//   const tabs = document.querySelectorAll(".tabs-title");
+//   tabs.forEach((tab) => tab.classList.remove("active"));
+
+//   const tabContents = document.querySelectorAll(".tabs-text");
+//   tabContents.forEach((content) => content.classList.remove("active"));
+
+//   tabs[tabIndex].classList.add("active");
+//   tabContents[tabIndex].classList.add("active");
+// }
+
 function switchTab(tabIndex) {
-  const tabs = document.querySelectorAll(".tabs-title");
-  tabs.forEach((tab) => tab.classList.remove("active"));
+  $(".tabs-title").removeClass("active");
+  $(".tabs-text").removeClass("active");
 
-  const tabContents = document.querySelectorAll(".tabs-text");
-  tabContents.forEach((content) => content.classList.remove("active"));
-
-  tabs[tabIndex].classList.add("active");
-  tabContents[tabIndex].classList.add("active");
+  $(".tabs-title").eq(tabIndex).addClass("active");
+  $(".tabs-text").eq(tabIndex).addClass("active");
 }
