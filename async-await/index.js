285d5efd03deac2ee7c5f6ab7cdf0e8ac6839553
LiliaBilous <liliyabilous.email@gmail.com>
2024-01-24 17:47:28 +0200

    added function filterUsers&eventListener

--- async-await/index.js
+++ async-await/index.js
@@ -9,11 +9,32 @@ async function renderList() {
     const userList = document.querySelector('.user-list');
     for(const user of users) {
         const userItem = document.createElement('li');
-        userItem.classList.add = 'user-list-item';
+        userItem.classList.add('user-list-item');
         userItem.innerHTML = user.name;
         userList.appendChild(userItem);
     }
 }
 
+function filterUsers(e) {
+    const usersListItems = document.querySelectorAll('.user-list-item');
+    const inputValue = e.target.value.toLowerCase();
+    
+    for (const user of usersListItems) {
+        const userName = user.innerHTML.toLowerCase();
+        if(!userName.includes(inputValue)) {
+            user.classList.add('hidden');
+        } else {
+            user.classList.remove('hidden');
+        }
+    }
+    if(inputValue === 0) {
+        usersListItems.forEach((item) => {
+            item.classList.remove('hidden');
+        });
+        return;
+    }
+}
 
-renderList()
+renderList();
+
+document.querySelector('.input').addEventListener('keyup', filterUsers)
